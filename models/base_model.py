0db6a4c782693681a7c93f6bf445f29dd178b00c
egbuniwecemeka <egbuniwecemeka.com>
2024-09-08 01:22:18 +0100

    created and updated public instance added

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,20 @@
 import uuid
+import datetime
 
 class BaseModel:
     def __init__(self):
             """Initializes instance attributes"""
             self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
 
-model = BaseModel().id
-model1 = BaseModel().id
+if __name__ == "__main__":
 
-print(model)
-print(model1)
+    model = BaseModel()
+    model1 = BaseModel()
+
+    print(model.id)
+    print(model.created_at)
+    print(model.updated_at)
+    print(model1)
