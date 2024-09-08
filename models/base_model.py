08aafa74fc4431aef77dd9d9283752c76ed55c65
egbuniwecemeka <egbuniwecemeka.com>
2024-09-08 23:40:22 +0100

    update_at and dynamic class name fix

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ class BaseModel:
             """Initializes public instance attributes"""
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
          """Updates current datetime anytime an object is changed"""
@@ -15,7 +15,7 @@ class BaseModel:
     
     def __str__(self):
          """Returns a string representation of BaseModel instance"""
-         return f"[{BaseModel}] ({self.id}) {self.__dict__}"
+         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
 
 if __name__ == "__main__":
