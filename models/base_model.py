f295a175c7c3e7f7f568b48336d208d606730e39
egbuniwecemeka <egbuniwecemeka.com>
2024-09-08 11:09:25 +0100

    object update tested

--- models/base_model.py
+++ models/base_model.py
@@ -2,11 +2,20 @@ import uuid
 import datetime
 
 class BaseModel:
+    """A base class for all common attributes/methods for other classes"""
     def __init__(self):
-            """Initializes instance attributes"""
+            """Initializes public instance attributes"""
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+
+    def save(self):
+         """Updates current datetime anytime an object is changed"""
+         self.updated_at = datetime.datetime.now()
+    
+    def __str__(self):
+         """Returns a string representation of BaseModel instance"""
+         return f"[{BaseModel}] ({self.id}) {self.__dict__}"
 
 
 if __name__ == "__main__":
@@ -14,7 +23,12 @@ if __name__ == "__main__":
     model = BaseModel()
     model1 = BaseModel()
 
+    print(model)
     print(model.id)
     print(model.created_at)
     print(model.updated_at)
-    print(model1)
+
+    model.save()
+
+    print(model.updated_at)
+    
