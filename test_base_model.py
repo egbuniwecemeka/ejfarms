338d48a0051482816dd5194b460d34d46f3a4084
egbuniwecemeka <egbuniwecemeka.com>
2024-09-10 10:42:48 +0100

    base model test deserialization

--- test_base_model.py
+++ test_base_model.py
@@ -15,3 +15,7 @@ print(f"\n{my_model_json}")
 print("JSON of my model:")
 for key in my_model_json.keys():
     print(f"\t{key}: {type(my_model_json[key])} - {my_model_json[key]}")
+
+print("--")
+new_model = BaseModel(**my_model_json)
+print(new_model.id)
