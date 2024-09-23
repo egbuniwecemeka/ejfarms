29e0e5961d6e57a22a2e0317643e64d28eb349dc
egbuniwecemeka <egbuniwecemeka.com>
2024-09-23 07:50:08 +0100

    mysql user setup edit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,8 +7,8 @@ USE `ejfarms_dev_db`;
 CREATE USER IF NOT EXISTS 'ej_dev'@'localhost' IDENTIFIED BY '$Fake_dev_password123';
 -- Grant all privileges on hbnb_dev_db to hbnb_dev
 -- 'WITH GRANRT OPTION' allows user to grant any inherited permission to other users
-GRANT ALL PRIVILEGES ON ejfarms_dev_db.* TO 'ej_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ejfarms_dev_db.* TO 'ej_dev'@'localhost';
 -- Grant SELECT privileges on performance_schema to hbnb_user
-GRANT SELECT ON 'performance_schema'.* TO 'ej_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'ej_dev'@'localhost';
 -- Apply changes
 FLUSH PRIVILEGES;
