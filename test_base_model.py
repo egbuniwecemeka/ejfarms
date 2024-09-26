f52eeea090d4cb532b5135c794c7c53cc6402cb3
egbuniwecemeka <egbuniwecemeka.com>
2024-09-26 01:47:54 +0100

    test updated timestamp of my_model instance

--- test_base_model.py
+++ test_base_model.py
@@ -6,7 +6,7 @@ my_model.name = "My First Model"
 my_model.my_number = 89
 print(my_model)
 my_model.save()
-print(my_model)
+print(f"\n{my_model}")
 """my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
