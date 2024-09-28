22c920344a943f423cce00cbc747143f3157451d
egbuniwecemeka <egbuniwecemeka.com>
2024-09-28 12:09:41 +0100

    test_base_model file edit

--- test_base_model.py
+++ test_base_model.py
@@ -10,6 +10,6 @@ print(f"\n{my_model}")
 my_model_json = my_model.to_dict()
 print(f"\n{my_model_json}")
 print("JSON of my_model:")
-for key in my_model_json.keys():
+for key in my_model_json.items():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
           my_model_json[key]))
